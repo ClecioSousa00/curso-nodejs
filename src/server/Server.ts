245bd1b568eb784619ae4aeb9bbfec1a73c82599
ClecioSousa00 <cleciosouza015@gmail.com>
2025-01-18 11:01:57 -0300

    Feat: aula 10

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import express from 'express'
 import { router } from './routes/index.js'
 
+import './shared/services/translateYup'
+
 const server = express()
 
 server.use(express.json())
--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,26 +1,38 @@
 import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import * as yup from 'yup'
 
 interface City {
   name: string
+  estado: string
 }
 
 const bodyValidation: yup.ObjectSchema<City> = yup.object({
   name: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 })
 
 export const create = async (req: Request<{}, {}, City>, res: Response) => {
   let validateData: City | undefined
   try {
-    validateData = await bodyValidation.validate(req.body)
+    validateData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    })
     console.log(validateData)
 
     res.send('Create!')
   } catch (error) {
     const yupError = error as yup.ValidationError
-    res.json({
+    const validationErrors: Record<string, string> = {}
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return
+      validationErrors[error.path] = error.message
+    })
+
+    res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
-        default: yupError.message,
+        default: validationErrors,
       },
     })
   }
